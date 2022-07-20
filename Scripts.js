d90bccc7f956b1e9cc0fbc0fb89cc0deed00ecaf
BenCosbey <107746880+BenCosbey@users.noreply.github.com>
2022-07-19 18:31:37 -0700

    Adding visuals
    created visuals and for and else statements

--- Scripts.js
+++ Scripts.js
@@ -1,5 +1,15 @@
 var pokemonList = [
     {name:'Bulbasaur', type: 'Grass', height:'2'},
     {name:'Charmander', type:'Fire',height:'2'},
-    {name:'Squirtle', type:'water', height:'1'} 
+    {name:'Squirtle', type:'Water', height:'1'},
+    {name:'Snorlax', type:'Normal', height:'9'}
 ];
+
+for (let i=0; i<pokemonList.length; i++) {
+    //writes special text for pokemons with a height over 1
+    if (pokemonList[i].height>6) {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow wow weee wow wow, its big!')
+    } else {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
+    }
+}

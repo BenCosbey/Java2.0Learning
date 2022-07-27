0fb6cee08b3c056e507be15a189ccb80fdcb56a1
BenCosbey <107746880+BenCosbey@users.noreply.github.com>
2022-07-26 18:34:54 -0700

    update 2.5
    added respository, forEach(), and pikachu

--- Scripts.js
+++ Scripts.js
@@ -1,15 +1,23 @@
-var pokemonList = [
-    {name:'Bulbasaur', type: 'Grass', height:'2'},
-    {name:'Charmander', type:'Fire',height:'2'},
-    {name:'Squirtle', type:'Water', height:'1'},
-    {name:'Snorlax', type:'Normal', height:'9'}
-];
 
-for (let i=0; i<pokemonList.length; i++) {
-    //writes special text for pokemons with a height over 1
-    if (pokemonList[i].height>6) {
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow wow weee wow wow, its big!')
-    } else {
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
-    }
-}
+    let pokemonRepository = (function(){
+
+        let pokemonList = [
+            {name:'Bulbasaur', type: 'Grass', height:'2'},
+            {name:'Charmander', type:'Fire',height:'2'},
+            {name:'Squirtle', type:'Water', height:'1'},
+            {name:'Snorlax', type:'Normal', height:'9'}
+        ];
+        return{
+            add: function(pokemon){
+                pokemonList.push(pokemon);
+            },
+            getAll: function(){
+                return pokemonList;
+            }
+        };
+    })();
+
+    console.log(pokemonRepository.getAll());
+    pokemonRepository.add({name:'pikachu'});
+    console.log(pokemonRepository.getAll());
+   

216914f17e9157d8651ace4f3a708bd5f6650a47
BenCosbey <107746880+BenCosbey@users.noreply.github.com>
2022-07-30 14:07:54 -0700

    1.6 update
    working on DOM, I need to display the array in HTML.

--- Scripts.js
+++ Scripts.js
@@ -1,23 +1,11 @@
 
-    let pokemonRepository = (function(){
-
-        let pokemonList = [
+        let Pokemon = [
             {name:'Bulbasaur', type: 'Grass', height:'2'},
             {name:'Charmander', type:'Fire',height:'2'},
             {name:'Squirtle', type:'Water', height:'1'},
             {name:'Snorlax', type:'Normal', height:'9'}
         ];
-        return{
-            add: function(pokemon){
-                pokemonList.push(pokemon);
-            },
-            getAll: function(){
-                return pokemonList;
-            }
-        };
-    })();
 
-    console.log(pokemonRepository.getAll());
-    pokemonRepository.add({name:'pikachu'});
-    console.log(pokemonRepository.getAll());
-   
+        Pokemon.forEach((Pokemon)=> {
+            document.write('${Pokemon.name}')
+        });
